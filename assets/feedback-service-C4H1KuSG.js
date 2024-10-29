1edc44966a6cba2309fe884db626ece98e4564f4
happyscreentime <localdevtools@gmail.com>
2024-10-29 22:32:56 +0530

    build files

--- assets/feedback-service-C4H1KuSG.js
+++
@@ -1,1 +0,0 @@
-import{e as a}from"./index-tTcqesqT.js";const t={triggerRequest:e=>a({...e,server:"db",collectionName:"feedback"}),create:({data:e})=>t.triggerRequest({method:"create",data:{...e,createdAt:new Date}}),getList:({data:e,...r}={})=>t.triggerRequest({method:"get",data:e,...r})};export{t as f};
