a29d9b01cb4d54ce5dfa835cee1ec8461a6ec112
happyscreentime <localdevtools@gmail.com>
2024-10-29 09:00:00 +0530

    build file

---
+++ assets/feedback-service-r3LbucSd.js
@@ -0,0 +1,1 @@
+import{e as a}from"./index-CYNFu8Jl.js";const t={triggerRequest:e=>a({...e,server:"db",collectionName:"feedback"}),create:({data:e})=>t.triggerRequest({method:"create",data:{...e,createdAt:new Date}}),getList:({data:e,...r}={})=>t.triggerRequest({method:"get",data:e,...r})};export{t as f};
